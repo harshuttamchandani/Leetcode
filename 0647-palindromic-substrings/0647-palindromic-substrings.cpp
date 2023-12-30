774bf16a7a923a52c945a71fdf068de0e6349f2b
Harsh Uttamchandani <harshuttamchandani2002@gmail.com>
2023-12-30 15:32:20 +0530

    Time: 595 ms (6.03%), Space: 457.5 MB (5.07%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int checkpallindrome(string t)
+        {
+        
+            
+            int i=0;
+            int j=t.length()-1;
+            while(i<=j)
+            {
+                if(t[i]!=t[j])
+                {
+                    return 0;
+                   
+                }
+                 i++;
+                j--;
+               
+            }
+        //cout<<t<<" ";
+             return 1;
+        }
+    
+    int countSubstrings(string s) {
+        int ans=0;
+        
+        
+        for(int i=0;i<s.length();i++)
+        {
+            string t="";
+            for(int j=i;j<s.length();j++)
+            {
+                t+=s[j];
+                //cout<<t<<" ";
+                int f=checkpallindrome(t);
+                if(f==1)
+                {
+                    //cout<<t<<" ";
+                    ans=ans+1;
+                    cout<<ans; 
+                }
+            }
+        }
+        return ans;
+    }
+};
